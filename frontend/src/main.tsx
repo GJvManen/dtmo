a18ab7ae64d17e532ed9e38c2a9a7208f1977f45
Godert Jan van Manen <gvmanen@gmail.com>
2026-08-20 15:08:48 +0200

    frontend: load command center styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 import './styles.css';
+import './command-center.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
