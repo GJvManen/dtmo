5929801b2091a9e2e9e2316769d2c2922beaf91b
Godert Jan van Manen <gvmanen@gmail.com>
2026-08-23 17:04:18 +0200

    Phase 11.10q: route canonical administration workspace

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { AdministrationWorkspace } from './AdministrationWorkspace';
 import { App } from './App';
 import './styles.css';
 import './command-center.css';
@@ -22,7 +23,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter basename="/workbench">
-        <App />
+        <Routes>
+          <Route path="/administration" element={<AdministrationWorkspace />} />
+          <Route path="*" element={<App />} />
+        </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
